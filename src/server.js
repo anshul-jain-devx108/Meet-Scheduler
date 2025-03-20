2d6dbb14afd66cf86c28f4b3eea4055454a51e08
ANSHUL JAIN <anshul.jain.devx@gmail.com>
2025-03-20 09:31:38 +0530

    Update server.js

--- src/server.js
+++ src/server.js
@@ -38,4 +38,4 @@ app.use((err, req, res, next) => {
 });
 
 const PORT = process.env.port || 5000;
-app.listen(PORT, () => console.log(`🚀 Server running on http://localhost:${PORT}`));
+app.listen(PORT,"0.0.0.0", () => console.log(`🚀 Server running on http://localhost:${PORT}`));
